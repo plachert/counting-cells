07b623beeebba9d11fd4c8e03748fd189ee3fc06
plachert <pmlachert@gmail.com>
2023-07-27 15:58:48 +0200

    Add BloodCellDataset, parse labels

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -1,15 +1,24 @@
 import pathlib
 from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
 
 
-class BloodCellDataset:
+class BloodCellDataset(Dataset):
     
     """https://www.kaggle.com/datasets/balakrishcodes/blood-cell-count-and-typesdetection"""
     
     def __init__(self, path: pathlib.Path, is_train: bool):
+        super().__init__()
         self.path = path
         self.train_or_valid = "train" if is_train else "valid" 
         self.labels = self._load_labels()
+        self.image_paths = self._load_image_paths()
+        
+        
+    def _load_image_paths(self):
+        paths = list(self.path.glob(f'images/images/{self.train_or_valid}/*.jpg'))
+        return paths
         
         
     def _load_labels(self):
@@ -24,11 +33,25 @@ class BloodCellDataset:
                 lines = file.readlines()
             labels[id_] = [parse_line(line) for line in lines] 
         return labels
+    
+    def _read_image(self, image_path):
+        image = np.array(Image.open(image_path))
+        return image
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        image = self._read_image(image_path)
+        id_ = image_path.stem
+        label = self.labels[id_]
+        return image, label
         
         
 if __name__ == "__main__":
     ds = BloodCellDataset(pathlib.Path("/home/piotr/datasets/vision/blood_cell_count_and_types_detection"), is_train=False)
-    print(ds.labels)
+    print(ds[0])
     
     
         
