62e7c75b5a2390f1385aab6939491ececd56d82e
plachert <pmlachert@gmail.com>
2023-07-28 14:57:57 +0200

    Longer fine tuning

--- examples/yolov8/fine_tune_yolo.py
+++ examples/yolov8/fine_tune_yolo.py
@@ -5,4 +5,4 @@ model = YOLO("yolov8n.yaml")
 model = YOLO("yolov8n.pt", task="detect")
 
 # Use the model
-model.train(data="./bcc_yolo_config.yaml", epochs=30)  # train the model
+model.train(data="./bcc_yolo_config.yaml", epochs=300)  # train the model
